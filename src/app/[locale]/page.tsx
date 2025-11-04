efaf7a18a8c418a58096accee58fa9265062af5e
Imcasero <diegocaserosmr@gmail.com>
2025-11-04 18:39:50 +0100

    fix: remove duplicate import of Image in page.tsx to clean up code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
-import Image from "next/image";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
