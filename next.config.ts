24fdc6cdc1606e653d0464decd24bd7e154a846f
Imcasero <diegocaserosmr@gmail.com>
2026-03-23 19:55:05 +0100

    chore: config for use react compiler and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   images: {
     formats: ["image/webp", "image/avif"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
