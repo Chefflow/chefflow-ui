2dcab2b7b3888dd9893323d2fd9256a729f1ba82
Imcasero <diegocaserosmr@gmail.com>
2026-05-07 22:23:32 +0200

    fix: update scrollarea styles for able scroll in recipe detail sidebar

--- src/components/BaseSidePanel/BaseSidePanel.tsx
+++ src/components/BaseSidePanel/BaseSidePanel.tsx
@@ -81,7 +81,7 @@ export const BaseSidePanel = ({
           </Button>
         </div>
 
-        <ScrollArea className="flex-1">
+        <ScrollArea className="flex-1 min-h-0">
           <div className="px-6 py-5 pb-12">{children}</div>
         </ScrollArea>
       </aside>
